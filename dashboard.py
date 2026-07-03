e090893eace6ba2c5ee5919578ee83846b13f69d
dannosD <dannos@seznam.cz>
2026-07-03 10:32:29 +0200

    uncommented

--- dashboard.py
+++ dashboard.py
@@ -331,7 +331,6 @@ class MouseTrackerDashboard(QMainWindow):
                         GPIO.output(17, GPIO.HIGH)
                         time.sleep(0.2)  # 100ms pulse
                         GPIO.output(17, GPIO.LOW)
-                        print("GPIO buzzer triggered.")
                     except Exception as e:
                         print(f"GPIO error: {e}")
                 elif platform.system() == "Windows":
